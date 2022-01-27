1473576d41f68d6820a514d5a8ba8924c5938053
WorldSkils <worldskils@78-36-158-177.dynamic.novgorod.dslavangard.ru>
2022-01-27 13:51:42 +0300

    com

---
+++ TestA/TestAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAApp.swift
+//  TestA
+//
+//  Created by WorldSkils on 27.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
